7ac4effa42af5574307e1ac9af314eb8276202a9
poonam9009 <ppoonia381@gmail.com>
2020-07-19 22:53:18 +0530

    Updates

---
+++ precache-manifest.90895f6c71d36e9571822b8eaa6361dc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467fd188d1f37680c31ae52bc2c5b406",
+    "url": "/poonam9009/portfolio.git/index.html"
+  },
+  {
+    "revision": "8fadb98e8a3c954ba553",
+    "url": "/poonam9009/portfolio.git/static/css/main.0a4d13ea.chunk.css"
+  },
+  {
+    "revision": "fcbd8120c83511c58792",
+    "url": "/poonam9009/portfolio.git/static/js/2.e02d3641.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poonam9009/portfolio.git/static/js/2.e02d3641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fadb98e8a3c954ba553",
+    "url": "/poonam9009/portfolio.git/static/js/main.7c10439b.chunk.js"
+  },
+  {
+    "revision": "132d3720a3d0818dabae",
+    "url": "/poonam9009/portfolio.git/static/js/runtime-main.999a97cb.js"
+  },
+  {
+    "revision": "a982db396ea4f22634d50e3139855513",
+    "url": "/poonam9009/portfolio.git/static/media/about.a982db39.JPEG"
+  },
+  {
+    "revision": "cacdde3cc6d4fd22e5f7b36cb7796f52",
+    "url": "/poonam9009/portfolio.git/static/media/bg.cacdde3c.jpg"
+  },
+  {
+    "revision": "3f2ef55e93a5a27cffe1726c7d701381",
+    "url": "/poonam9009/portfolio.git/static/media/bg111.3f2ef55e.jpg"
+  },
+  {
+    "revision": "8609d04d5986b169209900225135aaf7",
+    "url": "/poonam9009/portfolio.git/static/media/bg333.8609d04d.jpg"
+  },
+  {
+    "revision": "fa9a2d24ba239d9ba7fe6ba07998db84",
+    "url": "/poonam9009/portfolio.git/static/media/cf1.fa9a2d24.ico"
+  },
+  {
+    "revision": "0de407f367bdf66871bcb9a0b9d1e890",
+    "url": "/poonam9009/portfolio.git/static/media/cf2.0de407f3.ico"
+  },
+  {
+    "revision": "4bee98159fa72655500851ee518fe930",
+    "url": "/poonam9009/portfolio.git/static/media/so1.4bee9815.ico"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/poonam9009/portfolio.git/precache-manifest.90895f6c71d36e9571822b8eaa6361dc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/poonam9009/portfolio.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
